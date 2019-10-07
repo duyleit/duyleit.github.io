022040fa236a80910d32155938a501276b43b371
duyleit <jiahsinitdept>
2019-10-07 14:29:23 +0700

    edit background

--- 1.js
+++ 1.js
@@ -2,17 +2,17 @@
 $(function () {
     vitricuaedu = $('#khoikn').offset().top;
 
-    $('.kn').click(function () {
+    $('.nav_kn').click(function () {
       $('body,html').animate({scrollTop:vitricuaedu},700)
         return false;
          })
 
-    $('.da').click(function () {
+    $('.nav_da').click(function () {
         $('body,html').animate({scrollTop:$('#khoida').offset().top},700)
         return false;
     })
 
-    $('.ttlh').click(function () {
+    $('.nav_ttlh').click(function () {
         $('body,html').animate({scrollTop:$('#khoittlh').offset().top},700)
         return false;
     })
