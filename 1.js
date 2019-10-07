0f10d57a9a41190d401ac57f1cc170fe339fb516
duyleit <jiahsinitdept>
2019-10-07 09:27:34 +0700

    edit responsive menu

---
+++ 1.js
@@ -0,0 +1,19 @@
+﻿
+$(function () {
+    vitricuaedu = $('#khoikn').offset().top;
+
+    $('.kn').click(function () {
+      $('body,html').animate({scrollTop:vitricuaedu},700)
+        return false;
+         })
+
+    $('.da').click(function () {
+        $('body,html').animate({scrollTop:$('#khoida').offset().top},700)
+        return false;
+    })
+
+    $('.ttlh').click(function () {
+        $('body,html').animate({scrollTop:$('#khoittlh').offset().top},700)
+        return false;
+    })
+})
